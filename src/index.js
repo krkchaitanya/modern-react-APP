12d8856fa3b2771ba24fca50f12c164552526662
krk chaitanya <krkchaitanya42@gmail.com>
2018-02-28 13:31:58 -0600

    addReduxDevToolsExt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, applyMiddleware ,compose} from "redux";
 import { Provider } from "react-redux";
 
 
@@ -27,7 +27,9 @@ const logger=(store)=>{
     }
 }
 
-const store=createStore(reducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
